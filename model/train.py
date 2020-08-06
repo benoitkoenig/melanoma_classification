0d81f4c6df39cacce74e481ffbc61f1bf9b2df06
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-06 15:48:57 +0200

    Add ModelCheckpoint and ReduceLROnPlateau

--- model/train.py
+++ model/train.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.callbacks import CSVLogger
+from tensorflow.keras.callbacks import CSVLogger, ModelCheckpoint, ReduceLROnPlateau
 
 from melanoma_classification.model.model import model
 from melanoma_classification.model.data_generator import DataGenerator
@@ -9,6 +9,14 @@ validation_data = dataGenerator.getValidationSet()
 gen = dataGenerator.getTrainingDataGenerator()
 
 csv_logger = CSVLogger('logs/training.csv')
+model_checkpoint = ModelCheckpoint(
+  filepath='weights/weights',
+  save_weights_only=True,
+  monitor='val_loss',
+  mode='min',
+  save_best_only=True,
+)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=1e-6)
 
 model.fit(
   gen,
@@ -16,5 +24,5 @@ model.fit(
   steps_per_epoch=steps_per_epoch,
   validation_data=validation_data,
   validation_batch_size=batch_size,
-  callbacks=[csv_logger],
+  callbacks=[csv_logger, model_checkpoint, reduce_lr],
 )
