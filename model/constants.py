666a6d8eb9315789021a548014b0e134ed4558e8
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-12 09:52:41 +0200

    Update parameters

--- model/constants.py
+++ model/constants.py
@@ -2,8 +2,8 @@ total_train_rows = 33126
 positives=584
 negatives=32542
 
-test_half_size = 44
-batch_size = 10
+test_half_size = 104
+batch_size = 4
 epochs = 10
 
 train_half_size = positives - test_half_size
