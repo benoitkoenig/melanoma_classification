9beea6cfec82370f97f440e991baa2b03ad5bcf0
Benoit Koenig <koenigbenoit47@gmail.com>
2020-08-12 12:31:25 +0200

    Fix model.predict.py

--- model/predict.py
+++ model/predict.py
@@ -5,13 +5,13 @@ from melanoma_classification.model.model import model
 
 with open('data/test.csv') as csvFile:
   csvReader = csv.reader(csvFile, delimiter=',')
-  rows = [r for r in csvReader][1:21]
+  rows = [r for r in csvReader][1:]
 
 def savePredictions(predictions):
-  with open('submissions/latest.csv', 'w') as csvFile:
+  with open('submissions/latest_probs.csv', 'w') as csvFile:
     csvWriter = csv.writer(csvFile, delimiter=',')
     for entry in zip(rows, predictions):
-      csvWriter.writerow([entry[0][0], int(entry[1][1] > 0.95)])
+      csvWriter.writerow([entry[0][0], entry[1][1]])
 
 model.load_weights('weights/weights')
 dataGenerator = getTestDataGenerator(rows)
